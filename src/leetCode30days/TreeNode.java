98b653d674800a588abbe5599e14a10ce32c81a3
bavly <bavly500@gmail.com>
2020-06-04 18:17:19 +0200

    TreeNode Class

--- src/leetCode30days/TreeNode.java
+++ src/leetCode30days/TreeNode.java
@@ -2,4 +2,15 @@ package leetCode30days;
 
 public class TreeNode {
 
+         int val;
+         TreeNode left;
+        TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+
 }
