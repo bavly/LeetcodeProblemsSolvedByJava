0912b7a4b04dad48c1a998a6c0d8e07edd6983c5
bavly <bavly500@gmail.com>
2020-06-13 18:26:56 +0200

    Adding test for GroupAnagrams

--- src/leetCode30days/GroupAnagrams.java
+++ src/leetCode30days/GroupAnagrams.java
@@ -7,11 +7,13 @@ import java.util.Map;
 public class GroupAnagrams {
 
     public static void main(String[] args) {
-            
+            // still need to add test casces
+            String[] test = {"eat", "tea", "tan", "ate", "nat", "bat"};
+            System.out.println(groupAnagrams(test));
 
     }
     
-    public  List<List<String>> groupAnagrams(String[] strs) {
+    public static List<List<String>> groupAnagrams(String[] strs) {
         if (strs == null || strs.length == 0) return new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
